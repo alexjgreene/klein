18c04467625f01c2455bc726498450ec90968ed5
alexjgreene <alex_greene@mail.ru>
2018-04-09 16:05:39 +0700

    Changes

--- pages/hello.php
+++ pages/hello.php
@@ -1,5 +1,13 @@
 <?php
 return function ($request, $response, $service, $app) {
-	$name='world';
-    $service->render('views/hello.php',['name' => $name]);
+	$name = 'world';
+	$db = $app->db();
+	$users = $db->query('SELECT * FROM user')->fetchAll();
+    $service->render(
+		'views/hello.php',
+		[
+			'name' => $name,
+			'users' => $users		
+		]
+	);
 };
--- views/hello.php
+++ views/hello.php
@@ -1,1 +1,14 @@
 <div>Hello <?= htmlspecialchars ($this->name) ?>!</div>
+<ul>
+	<?php
+		foreach ($this->users as $user) {
+	?>
+    <li>
+		<?= htmlspecialchars(
+			$user['email']
+		) ?>
+	</li>
+	<?php 
+		}
+	?>
+</ul>
