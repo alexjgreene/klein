5c24d1c92f7d997ddef1d57d7b1ed8c7160d1af3
alexjgreene <alex_greene@mail.ru>
2018-04-09 14:34:32 +0700

    Adding pages

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$klein = new \Klein\Klein();
+
+$klein->respond(
+	'GET',
+	'/hello-world',
+	require('pages/hello.php')
+);
+
+$klein->respond(
+	'GET',
+	'/',
+	require('pages/main.php')
+);
+
+
+$klein->dispatch();
